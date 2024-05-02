1d76cc3a6ce4625b3d9bd01f6511a3fdebc6f769
Nikhil harchand <nikhilharchand@gmail.com>
2024-05-03 00:02:50 +0530

    Fix typo in Users.js model

--- src/models/Users.js
+++ src/models/Users.js
@@ -3,6 +3,6 @@ import mongoose from 'mongoose';
 const UserSchema=new monggose.Schema({
     username:{ type:String,required:true,unique:true},
     password:{ type:String,required:true}
-})
+});
 
 export const UserModel=mongoose.model('User',UserSchema);
